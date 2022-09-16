00fb5c83a6e750583888f9f664d833832fd22975
John Requizo <jereq1@student.monash.edu>
2022-09-16 21:03:48 +1000

    Complete neetcode 155 exercise

--- stack/155.py
+++ stack/155.py
@@ -10,3 +10,32 @@ int top() gets the top element of the stack.
 int getMin() retrieves the minimum element in the stack.
 You must implement a solution with O(1) time complexity for each function
 """
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        min = self.getMin()
+        if min is None or val < min:
+            self.stack.append((val, val))
+        else:
+            self.stack.append((val, min))
+
+    def pop(self) -> None:
+        if self.stack:
+            return self.stack.pop()[0]
+        return None
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1][0]
+        return None
+
+
+    def getMin(self) -> int:
+        if self.stack:
+            return self.stack[-1][1]
+        return None
+        
