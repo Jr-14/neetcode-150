4dd23a3d54c1f58c3794639a9fea41af5c90dc2f
John Requizo <jr.jemerson14@gmail.com>
2022-12-27 21:32:32 +1100

    Initial template for group anagrams problem

---
+++ arrays-and-hashing/49-group-anagrams.cpp
@@ -0,0 +1,22 @@
+/*
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Constraints:
+    1 <= strs.length <= 104
+    0 <= strs[i].length <= 100
+    strs[i] consists of lowercase English letters.
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    
+// }
+
+int main() {
+    return 0;
+}
