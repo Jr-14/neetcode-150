a7ba2f8d2d3b4c181b5e6381c4493ecec71481e8
John Requizo <jr.jemerson14@gmail.com>
2022-12-27 19:56:26 +1100

    complete solution

--- arrays-and-hashing/218-valid-anagram.cpp
+++ arrays-and-hashing/218-valid-anagram.cpp
@@ -2,16 +2,47 @@
 Given two strings 's' and 't', return true if 't' is an anagram of 's', and false otherwise.
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Constraints:
+    1 <= s.length, t.length <= 5 * 104
+    s and t consist of lowercase English letters.
 */
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
+unordered_map<char, int> mapString(string s) {
+    unordered_map<char, int> m;
+    for (char& c : s) {
+        m[c]++;
+    }
+    return m;
+}
+
 bool isAnagram(string s, string t) {
+    unordered_map<char, int> sMap = mapString(s);
+    unordered_map<char, int> tMap = mapString(t);
+    if (sMap.size() != tMap.size()) {
+        return false;
+    }
+
+    for (char& c : s) {
+        if (tMap.find(c) != tMap.end()) {
+            if (sMap[c] != tMap[c]) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
     return true;
 }
 
 int main() {
+    string a = "anagram";
+    string b = "nagaram";
+    bool isA = isAnagram(a, b);
+    cout << isA << endl;
     return 0;
 }
