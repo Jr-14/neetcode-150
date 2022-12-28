5b5cd8019b0f8c02e2476f09ddc79ca503c471ed
John Requizo <jr.jemerson14@gmail.com>
2022-12-28 15:49:50 +1100

    Complete solution using hashmaps

--- arrays-and-hashing/49-group-anagrams.cpp
+++ arrays-and-hashing/49-group-anagrams.cpp
@@ -13,10 +13,42 @@ Constraints:
 
 using namespace std;
 
-// vector<vector<string>> groupAnagrams(vector<string>& strs) {
-    
-// }
+map<char, int> mapString(string s) {
+    map<char, int> m;
+    for (char& c : s) {
+        m[c]++;
+    }
+    return m;
+}
+
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    map<map<char, int>, vector<string>> mappings;
+    for (string& s : strs) {
+        map<char, int> ms = mapString(s);       
+        if (mappings.find(ms) != mappings.end()) {
+            vector<string> vs = mappings[ms];
+            vs.push_back(s);
+            mappings[ms] = vs;
+        } else {
+            vector<string> vs;
+            vs.push_back(s);
+            mappings[ms] = vs;
+        }
+    }
+
+    vector<vector<string>> st;
+    for (const auto& [k, v] : mappings) {
+        vector<string> commonAnagrams;
+        for (string s : v) {
+            commonAnagrams.push_back(s);
+        }
+        st.push_back(commonAnagrams);
+    }
+    return st;
+}
 
 int main() {
+    vector<string> strings = {"eat","tea","tan","ate","nat","bat"};
+    vector<vector<string>> ga = groupAnagrams(strings);
     return 0;
 }
