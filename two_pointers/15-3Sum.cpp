0176824a6f885261d95b3873fc8ee38023e90e71
John Requizo <jr.jemerson14@gmail.com>
2023-03-28 22:23:54 +1100

    Initial commit

---
+++ two_pointers/15-3Sum.cpp
@@ -0,0 +1,21 @@
+/*
+*
+* Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such 
+* that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+*
+* Notice that the solution set must not contain duplicate triplets.
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+        
+}
+
+int main() {
+
+}
+
