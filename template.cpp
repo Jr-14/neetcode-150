ec6008ee7d75ac7d12c22a626f2c69cdb24026a2
John Emerson Requizo <jr.jemerson14@gmail.com>
2024-03-22 08:25:20 +1100

    Add competitive programming template

---
+++ template.cpp
@@ -0,0 +1,39 @@
+#include <ios>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <stack>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+
+using namespace std;
+
+#define ll long long
+
+/**
+ * Speed up cin and cout
+ */
+void Speed() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+}
+
+void solve() {
+
+}
+
+
+int main() {
+  Speed();
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
