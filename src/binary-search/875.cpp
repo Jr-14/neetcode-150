fc5dce466be74b11b7cc8f9b2b14f9385b81a52f
John Emerson Requizo <jr.jemerson14@gmail.com>
2025-01-12 20:44:04 +1100

    Complete leetcode #875 koko eating bananas

--- src/binary-search/875.cpp
+++ src/binary-search/875.cpp
@@ -14,34 +14,32 @@ void Speed() {
     cout.tie(NULL);
 }
 
-bool comp(int a, int b) {
-    return a < b;
-}
+int minEatingSpeed(vector<int> &piles, int h) {
+    int max = *max_element(piles.begin(), piles.end());
 
-int minEatingSpeed(vector<int> &piles, int target) {
-    sort(piles.begin(), piles.end(), comp);
-    for (auto p : piles) {
-        cout << p << endl;
-    }
-
-    int l = 0, u = piles.size() - 1;
-    while (l <= u) {
-        int k = (l + u)/2;
-        if (piles[k] == target) {
-            return true;
+    int lower = 1, upper = max;
+    int answer = -1;
+    while (lower <= upper) {
+        int mid = lower + (upper - lower)/2;
+        long long totalHours = 0;
+        for (auto pile : piles) {
+            totalHours += ceil((double)pile/mid);
         }
-        if (piles[k] > target) {
-            u = k - 1;
+
+        if (totalHours <= h) {
+            answer = mid;
+            upper = mid - 1;
         } else {
-            l = k + 1;
+            lower = mid + 1;
         }
     }
-    return false;
+    return answer;
 }
 
 void solve() {
-    vector<int> v = {30,11,23,4,20};
-    cout << minEatingSpeed(v, 3) << endl;
+    // vector<int> v = {30,11,23,4,20};
+    vector<int> v = {312884470};
+    cout << minEatingSpeed(v, 968709470) << endl;
 }
 
 
