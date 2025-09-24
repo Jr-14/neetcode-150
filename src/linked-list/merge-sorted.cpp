f01aba43d29634941d7e817052f474b3d4f9c80c
John Emerson Requizo <jr.jemerson14@gmail.com>
2025-09-24 21:07:06 +1000

    Merged initial file

---
+++ src/linked-list/merge-sorted.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/**
+ * Speed up cin and cout
+ */
+void Speed() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void solve() {
+
+}
+
+ListNode* mergedTwoLists(ListNode* listOne, ListNode* listTwo) {
+
+}
+
+
+int main() {
+    Speed();
+    int t = 1;
+    solve();
+    return 0;
+}
