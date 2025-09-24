04778f56326c0bd4197b48ad9b5797f0e0f4c493
John Emerson Requizo <jr.jemerson14@gmail.com>
2025-09-24 23:11:08 +1000

    initial commit of reoder linked list

---
+++ src/linked-list/reorder.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/**
+ * Speed up cin and cout
+ */
+void Speed() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void solve() {
+
+}
+
+/**
+ * Time complexity O(n)
+ * Space complexity O(1)
+ */
+void reorderList(ListNode* head) {
+
+}
+
+
+int main() {
+    Speed();
+    int t = 1;
+    solve();
+    return 0;
+}
+
